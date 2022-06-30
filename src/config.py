dd3e5e4b1c840511426c618cf05ed99aad0f021b
Aniket Teredesai <aniket.teredesai@gmail.com>
2022-06-30 14:06:33 +0530

    reorganize

--- src/config.py
+++ src/config.py
@@ -6,12 +6,12 @@ class Config:
 
     use_gradient: bool = True  # use gradient images
 
-    # paths to images (relative to working dir)
-    base_gradient: str = "base_gradient.png"
-    arc_gradient: str = "arc_gradient.png"
+    # paths to images (relative to base dir)
+    base_gradient: str = "assets/base_gradient.png"
+    arc_gradient: str = "assets/arc_gradient.png"
 
     # hex colors if `use_gradient` is set to false
     base_clr: str = "#000000"
     arc_clr: str = "#ffffff"
 
-    font_file: str = "poppins_regular.ttf"  # path to a font-file
+    font_file: str = "assets/poppins_regular.ttf"  # path to a font-file
---
+++ src/local.py
@@ -0,0 +1,20 @@
+from image import create_slice, composite_avatar
+from config import Config
+
+angle = 234
+txt = "65%"
+og_avatar = "assets/raw_avatar.jpg"
+
+slice_img, slice_mask = create_slice(
+    angle,
+    txt,
+    txt_color=Config.txt_color,
+    font_file=Config.font_file,
+    gradient=Config.arc_gradient,
+)
+
+composite_img = composite_avatar(
+    og_avatar, slice_img=slice_img, slice_mask=slice_mask, gradient=Config.base_gradient
+)
+
+composite_img.save("processed_avatar.png", "PNG")
